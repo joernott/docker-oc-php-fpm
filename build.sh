3b485d0665c4d4d5959bda1dceef7ab19f879186
Joern Ott <joern.ott@ott-consult.de>
2017-11-16 07:58:30 +0000

    Fix wrong pushes in build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -e
 
 docker build -f Dockerfile56 -t registry.ott-consult.de/oc/php-fpm:5.6 .
-docker push registry.ott-consult.de/oc/apache-php:5.6
+docker push registry.ott-consult.de/oc/php-fpm:5.6
 docker build -f Dockerfile70 -t registry.ott-consult.de/oc/php-fpm:7.0 .
-docker push registry.ott-consult.de/oc/apache-php:7.0
+docker push registry.ott-consult.de/oc/php-fpm:7.0
 docker build -f Dockerfile71 -t registry.ott-consult.de/oc/php-fpm:7.1 .
 docker tag registry.ott-consult.de/oc/php-fpm:7.1 registry.ott-consult.de/oc/php-fpm:latest
 docker push registry.ott-consult.de/oc/php-fpm:7.1
