d629930acbaeec31d4a2f79da83f8b66d061790b
Jörn Ott <joern.ott@ott-consult.de>
2018-01-14 22:33:44 +0100

    Add label, track image history

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
+set -x
+curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
+source src/tmp/install/functions.sh
 
+patch_dockerfile Dockerfile56
 docker build -f Dockerfile56 -t registry.ott-consult.de/oc/php-fpm:5.6 .
 docker push registry.ott-consult.de/oc/php-fpm:5.6
+
+patch_dockerfile Dockerfile70
 docker build -f Dockerfile70 -t registry.ott-consult.de/oc/php-fpm:7.0 .
 docker push registry.ott-consult.de/oc/php-fpm:7.0
+
+patch_dockerfile Dockerfile71
 docker build -f Dockerfile71 -t registry.ott-consult.de/oc/php-fpm:7.1 .
 docker tag registry.ott-consult.de/oc/php-fpm:7.1 registry.ott-consult.de/oc/php-fpm:latest
 docker push registry.ott-consult.de/oc/php-fpm:7.1
--- src/tmp/install/oc-php-fpm.sh
+++ src/tmp/install/oc-php-fpm.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-curl -sSo /tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
 source /tmp/install/functions.sh
 add_repos IUS EPEL
 
