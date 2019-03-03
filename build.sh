1401da7fd509aa721c62194e2a5f34577f666c7d
Joern Ott <joern.ott@ott-consult.de>
2019-03-03 19:59:07 +0000

    Remove old versions

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -x
 curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
 source src/tmp/install/functions.sh
 
-patch_dockerfile Dockerfile56
-docker build -f Dockerfile56 -t registry.ott-consult.de/oc/php-fpm:5.6 .
-docker push registry.ott-consult.de/oc/php-fpm:5.6
+patch_dockerfile Dockerfile71
+docker build -f Dockerfile71 -t registry.ott-consult.de/oc/php-fpm:7.1 .
+docker push registry.ott-consult.de/oc/php-fpm:7.1
 
 patch_dockerfile Dockerfile72
 docker build -f Dockerfile72 -t registry.ott-consult.de/oc/php-fpm:7.2 .
